f60b4de5d2957c8b56ba561df7ae25e46ca273b0
Srija Arumalla <srijaarumalla926@gmail.com>
2020-07-30 09:42:50 +0530

    Solution

--- Module3/app.js
+++ Module3/app.js
@@ -10,7 +10,7 @@
     function FoundItems() {
         var ddo = {
             restrict: 'E',
-            templateUrl: 'foundItems.html',
+            templateUrl: 'founditem.html',
             scope: {
                 foundItems: '<',
                 onEmpty: '<',
